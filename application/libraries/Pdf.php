438807dd9d332fd085c3d6b956ef61ffab9f856a
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2022-08-18 00:42:26 +0700

    merge

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once(dirname(__FILE__) . '/libraries/dompdf/autoload.inc.php');
+require_once(dirname(__FILE__) . '/dompdf/autoload.inc.php');
 // require_once(_MAP."libraries/dompdf/autoload.inc.php");
 
 class Pdf
